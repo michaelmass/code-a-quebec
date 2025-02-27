45539b5af78c8d390e43060793857c6800163261
michaelmass <10071388+michaelmass@users.noreply.github.com>
2025-02-27 01:29:25 -0500

    update

--- 6-dagger-deno/index.ts
+++ 6-dagger-deno/index.ts
@@ -4,4 +4,16 @@ await connect(async (client) => {
 	const source = client
 		.host()
 		.directory("./app", { exclude: ["node_modules/"] });
+
+	const bunFile = client
+		.container()
+		.from("oven/bun")
+		.withDirectory("/src", source)
+		.withWorkdir("/src")
+		.withExec(["bun", "install"])
+		.withExec(["bun", "run", "build:bun"])
+		.directory("dist/")
+		.file("index.js");
+
+	console.log(await bunFile.sync());
 });
