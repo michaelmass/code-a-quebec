f8cba22b11a71026fb525486d20f193ad2d678b9
michaelmass <10071388+michaelmass@users.noreply.github.com>
2025-02-27 02:26:22 -0500

    update

---
+++ 7-dagger-botpress/index.ts
@@ -0,0 +1,41 @@
+import {
+	type Container,
+	connect,
+} from "https://esm.sh/@dagger.io/dagger@0.9.6";
+
+await connect(async (client) => {
+	const repository = client
+		.container()
+		.from("alpine")
+		.withExec(["apk", "add", "git"], { skipEntrypoint: true })
+		.withExec(["git", "clone", "https://github.com/botpress/botpress"], {
+			skipEntrypoint: true,
+		})
+		.directory("botpress");
+
+	const integrationsDirectory = repository.directory("integrations");
+	const integrations = await integrationsDirectory.entries();
+
+	const container = await client
+		.container()
+		.from("node:18-bullseye")
+		.withExec(["npm", "install", "-g", "pnpm@8"], { skipEntrypoint: true })
+		.withDirectory("/src", repository)
+		.withWorkdir("/src")
+		.withExec(["pnpm", "install"], { skipEntrypoint: true })
+		.withExec(["pnpm", "build"], { skipEntrypoint: true })
+		.sync();
+
+	const promises: Promise<Container>[] = [];
+
+	for (const integration of integrations) {
+		const promise = container
+			.withWorkdir(`/src/integrations/${integration}`)
+			.withExec(["pnpm", "check:type"], { skipEntrypoint: true })
+			.sync();
+
+		promises.push(promise);
+	}
+
+	await Promise.all(promises);
+});
