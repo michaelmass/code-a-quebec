f60818d170971a466d394879f04630152eb519af
michaelmass <10071388+michaelmass@users.noreply.github.com>
2025-06-25 01:19:20 -0400

    update

---
+++ website/src/components/Number.tsx
@@ -0,0 +1,13 @@
+type NumberTitleProps = {
+	number: string;
+};
+
+export const NumberTitle = ({ number }: NumberTitleProps) => {
+	return (
+		<span className="inline-block">
+			<div className="font-mono rounded-4xl px-3 py-2 flex items-center justify-center text-base bg-blue-700 text-white">
+				{number}
+			</div>
+		</span>
+	);
+};
