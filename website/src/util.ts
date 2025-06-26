6cba2e8aef414d048191bd806818ec6d37bb40fd
Michael Masson <10071388+michaelmass@users.noreply.github.com>
2025-06-26 02:28:00 -0400

    refactor(website): dynamic event link generation (#9)

--- website/src/util.ts
+++ website/src/util.ts
@@ -15,3 +15,25 @@ export const formatRelativeDate = (date: Date | string) => {
 
   return `Dans ${diffInDays} jours` // e.g. "9 June 2025"
 }
+
+export const getLastThursdayOfMonth = (date = new Date()) => {
+  const nextThursday = new Date(date)
+
+  while (nextThursday.getDay() !== 4) {
+    nextThursday.setDate(nextThursday.getDate() + 1)
+  }
+
+  const lastThursday = new Date(nextThursday.getFullYear(), nextThursday.getMonth() + 1, 0)
+
+  while (lastThursday.getDay() !== 4) {
+    lastThursday.setDate(lastThursday.getDate() - 1)
+  }
+
+  lastThursday.setHours(18, 0, 0, 0)
+
+  return lastThursday
+}
+
+export const formatCodeAQuebecLink = (date: Date) => {
+  return `https://code-a-quebec-${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}.eventbrite.ca`
+}
