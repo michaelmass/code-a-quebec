c5bc9e968454b237714ec3a01f0ac7602f1adeb0
Mathys <mathys.deshaies@icloud.com>
2025-06-29 23:27:03 -0400

    feat: add new grid section to home page

--- website/src/app/(main)/page.tsx
+++ website/src/app/(main)/page.tsx
@@ -1,11 +1,13 @@
 import { Hero } from "@/components/Hero";
 import { Newsletter } from "@/components/Newsletter";
 import { Speakers } from "@/components/Speakers";
+import { Grid } from "@/components/Grid";
 
 export default function () {
   return (
     <>
       <Hero />
+      <Grid />
       <Speakers />
       <Newsletter />
     </>
