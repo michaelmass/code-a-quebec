b94915025acb730003cddff091b8be6342948ab2
michaelmass <10071388+michaelmass@users.noreply.github.com>
2025-08-11 22:34:30 -0400

    update

--- website/src/components/Logo.tsx
+++ website/src/components/Logo.tsx
@@ -1,14 +1,7 @@
-import Image from "next/image";
-import LogoSvg from "@/images/logos/logo.png";
-import { cn } from "@/util";
+import Image from 'next/image'
+import LogoSvg from '@/images/logos/logo.png'
+import { cn } from '@/util'
 
-export function Logo(props: Omit<React.ComponentProps<typeof Image>, "src" | "alt">) {
-  return (
-    <Image
-      {...props}
-      alt="Logo"
-      src={LogoSvg.src}
-      className={cn("rounded-full bg-blue-700 shadow-xl", props.className)}
-    />
-  );
+export function Logo(props: Omit<React.ComponentProps<typeof Image>, 'src' | 'alt'>) {
+  return <Image {...props} alt="Logo" src={LogoSvg.src} className={cn('rounded-full bg-blue-700 shadow-xl', props.className)} />
 }
--- website/src/components/Share.tsx
+++ website/src/components/Share.tsx
@@ -1,24 +1,21 @@
-import { Popover, PopoverButton, PopoverPanel } from "@headlessui/react";
-import Link from "next/link";
-import LinkedInLogo from "@/images/logos/linkedin.svg";
-import XLogo from "@/images/logos/x.svg";
-import { cn } from "@/util";
+import { Popover, PopoverButton, PopoverPanel } from '@headlessui/react'
+import Link from 'next/link'
+import LinkedInLogo from '@/images/logos/linkedin.svg'
+import XLogo from '@/images/logos/x.svg'
+import { cn } from '@/util'
 
-const codeAQuebecUrl = "https%3A%2F%2Fcode-a-quebec.com";
+const codeAQuebecUrl = 'https%3A%2F%2Fcode-a-quebec.com'
 
 type ShareProps = {
-  children: React.ReactNode;
-  className?: string;
-};
+  children: React.ReactNode
+  className?: string
+}
 
 export const Share = ({ children, className }: ShareProps) => {
   return (
-    <Popover className={cn("relative", className)}>
+    <Popover className={cn('relative', className)}>
       <PopoverButton>{children}</PopoverButton>
-      <PopoverPanel
-        anchor={{ to: "top", gap: "8px" }}
-        className="flex rounded-xl bg-white p-2 shadow"
-      >
+      <PopoverPanel anchor={{ to: 'top', gap: '8px' }} className="flex rounded-xl bg-white p-2 shadow">
         <div className="flex flex-col gap-1">
           <div className="text-gray-400">Partage</div>
           <div className="flex items-center justify-center gap-2">
@@ -30,17 +27,12 @@ export const Share = ({ children, className }: ShareProps) => {
             >
               <LinkedInLogo className="inline-block" height="16" width="16" viewBox="0 0 24 24" />
             </Link>
-            <Link
-              href={`https://x.com/intent/post?url=${codeAQuebecUrl}`}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="inline-block font-light text-gray-400 hover:text-gray-500"
-            >
+            <Link href={`https://x.com/intent/post?url=${codeAQuebecUrl}`} target="_blank" rel="noopener noreferrer" className="inline-block font-light text-gray-400 hover:text-gray-500">
               <XLogo className="inline-block" height="14" width="14" viewBox="0 0 32 32" />
             </Link>
           </div>
         </div>
       </PopoverPanel>
     </Popover>
-  );
-};
+  )
+}
