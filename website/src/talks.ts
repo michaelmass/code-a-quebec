eb22c83a6cd03ba876f0a3fa3d8056bd061236bf
michaelmass <10071388+michaelmass@users.noreply.github.com>
2025-08-18 08:44:17 -0400

    update

--- website/src/talks.ts
+++ website/src/talks.ts
@@ -176,7 +176,7 @@ export const events: Event[] = [
       {
         profile: profiles.francoisLevasseur,
         title: 'Remote Code Execution - Comment éviter de que ton produit devienne une plateforme de Bitcoin mining',
-        summary: '',
+        summary: 'La présentation de Fleur était instructive. Il a démontré une nouvelle facade du "Remote Code Execution" (RCE). Évidemment, le RCE est perçu comme un exploit de sécurité. Par contre, si c\'est exactement ce qu\'on veut permettre, il faut trouver des techniques sécuritaires pour faire du RCE. C\'est exactement ce que Fleur explique dans sa présentation. Il nous explique les différentes approches et technologies que l\'on peut utiliser tel que Docker, gvisor, firecracker, V8 Isolates et VRL. ',
         slidesUrl: 'https://github.com/michaelmass/code-a-quebec/raw/refs/heads/master/talks/2025-07-31/remote-code-execution/slides.pptx',
         links: [
           {
